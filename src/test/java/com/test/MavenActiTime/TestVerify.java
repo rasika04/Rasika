35232e8d04d6802a15928998b5bddc5f7ff4a89e
HP <HP@HP-PC>
2020-05-29 18:14:09 +0530

    New class added

---
+++ src/test/java/com/test/MavenActiTime/TestVerify.java
@@ -0,0 +1,12 @@
+package com.test.MavenActiTime;
+
+import org.testng.annotations.Test;
+
+public class TestVerify 
+{
+   @Test
+   public void verify()
+   {
+	   System.out.println("verify");
+   }
+}
